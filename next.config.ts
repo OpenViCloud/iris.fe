00f58d787b437af5da4c20c10656c51e30afebce
lieudvq0302 <lieudvq0302@gmail.com>
2025-12-25 11:15:21 +0700

    Config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  images: {
+    unoptimized: true,
+  },
+
+  reactStrictMode: false,
 };
 
 export default nextConfig;
